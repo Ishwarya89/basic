6338e6a49e2078ba08b5ea1ecd4acd9fac92c1a9
Ishwarya <ishbodi2017@gmail.com>
2023-11-10 18:55:14 +0530

    first commit

---
+++ src/Component/Basic/Basic.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = () => {
+  return (
+    <div>
+    <h1>hai</h1>
+
+    </div>
+  )
+}
+
+export default Basic
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Basic from './Component/Basic/Basic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Basic/>
   </React.StrictMode>
 );
 
